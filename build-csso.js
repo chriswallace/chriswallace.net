6083a595343b714d61211685c12b5aac57a30c85
Chris Wallace <chriswallace7@protonmail.com>
2023-10-27 14:27:27 -0500

    update build script

--- build-csso.js
+++ build-csso.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 
 const css = fs.readFileSync('./assets/main.css', 'utf8');
 const minifiedCss = csso.minify(css).css;
-fs.writeFileSync('./_site/assets/main.css', minifiedCss);
+fs.writeFileSync('./assets/main.css', minifiedCss);
